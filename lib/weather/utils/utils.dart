3c3077576b33125e7b0869a05870e0e35047dded
Euclides Catunda <euclides.catunda@grupoboticario.com.br>
2023-08-20 22:30:55 -0300

    fiz date format

--- lib/weather/utils/utils.dart
+++ lib/weather/utils/utils.dart
@@ -2,7 +2,7 @@ class Utils {
   static String convertTimestampToDateString(int timestamp) {
     final DateTime date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
     final String formattedDate =
-        "${date.day}/${date.month}/${date.year} ${date.hour}:${date.minute}:${date.second}";
+        "${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year} ${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}:${date.second.toString().padLeft(2, '0')}";
     return formattedDate;
   }
 }
